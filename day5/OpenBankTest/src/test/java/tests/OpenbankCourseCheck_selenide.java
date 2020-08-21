3aae22dfed74d6267834de13e1cc1ed35d8817c5
Nika Romane <nikatina93@yandex.ru>
2020-08-21 13:05:37 +0300

    openbank selenide add timeout

--- day5/OpenBankTest/src/test/java/tests/OpenbankCourseCheck_selenide.java
+++ day5/OpenBankTest/src/test/java/tests/OpenbankCourseCheck_selenide.java
@@ -6,13 +6,15 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class OpenbankCourseCheck_selenide {
         @Test
-        public void testDollarBuyMoreThanSale() {
+        public void testDollarBuyMoreThanSale() throws InterruptedException {
+                Thread.sleep(500);
                 openBankPage obp = open("https://www.open.ru", openBankPage.class);
                 Assert.assertFalse(obp.getDollarBuy() > obp.getDollarSale());
         }
 
         @Test
-        public void testEuroBuyMoreThanSale() {
+        public void testEuroBuyMoreThanSale() throws InterruptedException {
+                Thread.sleep(500);
                 openBankPage obp = open("https://www.open.ru", openBankPage.class);
                 Assert.assertFalse(obp.getEuroBuy() > obp.getEuroSale());
 
